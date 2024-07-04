35a9415d3c1c6304e30562e3327558f40fbf15e6
Laila <lailalinke95@gmail.com>
2024-07-04 11:47:21 +0200

    Added main files

---
+++ niceplots/__init__.py
@@ -0,0 +1,8 @@
+"""
+niceplots
+
+Code for creating nice matplotlib plots
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Laila Linke'
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='niceplots',
+    version='0.0.1',
+    description=' Code for creating nice matplotlib plots',
+    url='',
+    author='Laila Linke',
+    author_email='laila.linke@uibk.ac.at',
+    packages=['niceplots'],
+    install_requires=['matplotlib'],
+    classifiers=[
+        'Development Status :: 2 - PreAlpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Astronomy',
+    ],
+)
